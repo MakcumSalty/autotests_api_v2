18f6bf9fb24aaec92c4f668067e8715ce589a41a
MakcumSalty <maksisaltey1991@gmail.com>
2026-09-19 20:21:24 +0300

    Применяем Pydantic в API клиентах

---
+++ clients/users/users_schema.py
@@ -0,0 +1,63 @@
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
+
+
+# Добавили описание структуры пользователя
+class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+# Добавили описание структуры ответа получения пользователя
+class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа получения пользователя.
+    """
+    user: UserSchema
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+# Добавили описание структуры ответа получения пользователя
+class GetUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа получения пользователя.
+    """
+    user: UserSchema
+
+
+class UpdateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на обновление пользователя.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    email: EmailStr | None
+    last_name: str | None = Field(alias="lastName")
+    first_name: str | None = Field(alias="firstName")
+    middle_name: str | None = Field(alias="middleName")
+
+
+class UpdateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа обновления пользователя.
+    """
+    user: UserSchema
