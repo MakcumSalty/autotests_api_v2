3b32ecfc4f791530d82383c3d67c8cb8612b7884
MakcumSalty <maksisaltey1991@gmail.com>
2026-09-21 21:39:04 +0300

    Маркировки Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,4 +1,7 @@
 from http import HTTPStatus
+
+import pytest
+
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.users.public_users_client import get_public_users_client
@@ -8,6 +11,8 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
 
+@pytest.mark.regression
+@pytest.mark.authentication
 def test_login():
     # Инициализируем API-клиент для работы с пользователями
     public_users_client = get_public_users_client()
@@ -29,7 +34,3 @@ def test_login():
     assert_login_response(login_response_data)
 
     validate_json_schema(login_response.json(), login_response_data.model_json_schema())
-
-
-
-
